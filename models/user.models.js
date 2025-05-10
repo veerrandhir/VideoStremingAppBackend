fec5c62736c8288e29101bcb2239190c1089d80e
Randhir Kumar <veerrandhir3@gmail.com>
2025-05-10 12:55:40 +0530

    fix: Correct import statement syntax and define user schema in user model

--- models/user.models.js
+++ models/user.models.js
@@ -1,2 +1,23 @@
-import mongoose from ("mongoose");
+import mongoose from "mongoose"; // two way 1. import 2. const require format
 
+// defined userSchema
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      required: true,
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      toLowerCase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamps: true }
+); // timestamps created at and updated at helps to get created and updated time.
+
+export const User = mongoose.model("User", userSchema); // User defined here will be accessed on db as "users" always plural.
